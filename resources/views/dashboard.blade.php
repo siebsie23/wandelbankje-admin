73fe19c347e7569e4bba5c374e96d85ad248f7e4
Sibren <sibrenvansetten@hotmail.nl>
2022-12-20 19:38:33 +0100

    Updates basic dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="text-2xl">
+                        Welcome to your dashboard
+                    </div>
+
+                    <div class="mt-6 text-gray-500">
+                        This is the Wandelbankje dashboard. Here you can view all statistics, benches, reviews, images etc. and manage them.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
