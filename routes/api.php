f0b379357f58b5af01e542b801727ffec282df9d
Sibren <sibrenvansetten@hotmail.nl>
2023-01-16 20:42:18 +0100

    Added simple api route to fetch benches in radius from point

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Bench;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use MatanYadaev\EloquentSpatial\Objects\Point;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+# Get benches in radius from longitude and latitude
+Route::get('/bench/{longitude}/{latitude}/{radius}', function ($longitude, $latitude, $radius) {
+    $benches = Bench::
+        whereDistanceSphere('coordinates', new Point($latitude, $longitude), '<', $radius)
+        ->get();
+    return $benches;
 });
